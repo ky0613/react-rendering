f19d177b2025c0e1c0aa3a70460416919161691e
ky0613 <95160084+ky0613@users.noreply.github.com>
2022-07-15 12:00:22 +0900

    useCallbackを用いて最適化

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { ChildArea } from "./ChildArea";
 import "./styles.css";
 
 // 再レンダリングが起きる条件
 // stateが変更されたコンポーネントは再レンダリング
-// propsが更新されたコンポーネントは再レンダリング
+// propsが更新されたコンポーネントは再レンダリング(memoによる最適化)
 // 再レンダリングされたコンポーネント配下の子要素は再レンダリング
 export default function App() {
   console.log("App");
@@ -12,14 +12,18 @@ export default function App() {
   const [open, setOpen] = useState(false);
 
   const onChengeText = (e) => setText(e.target.value);
+
   const onClickOpen = () => setOpen(!open);
+  // アロー関数で書いたときはコンポーネントをmemo化しても新しい別の関数と認識されるので
+  // propsの変更を感知して再レンダリングが起きる
+  const onClickClose = useCallback(() => setOpen(false), []);
   return (
     <div className="App">
       <input type="text" onChange={onChengeText} value={text} />
       <br />
       <br />
       <button onClick={onClickOpen}>表示</button>
-      <ChildArea open={open} />
+      <ChildArea open={open} onClickClose={onClickClose} />
     </div>
   );
 }
--- src/ChildArea.jsx
+++ src/ChildArea.jsx
@@ -7,7 +7,8 @@ const style = {
 };
 
 // memoを使うことでpropsが変更されたときのみ再レンダリングする挙動になる
-export const ChildArea = memo(({ open }) => {
+// 規模の大きいコンポーネントには基本的にmemoを使用する
+export const ChildArea = memo(({ open, onClickClose }) => {
   console.log("ChildAreaがレンダリングされた！！");
   const data = [...Array(2000).keys()];
   data.forEach(() => {
@@ -19,6 +20,7 @@ export const ChildArea = memo(({ open }) => {
       {open ? (
         <div style={style}>
           <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
         </div>
       ) : null}
     </>
